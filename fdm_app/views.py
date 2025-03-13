58b775c1ad1d1087652859df41aec0666a6996a0
Mihajaz <mihajarazafimahazoson@gmail.com>
2025-03-13 16:02:33 +0300

    integration of the search bar

--- fdm_app/views.py
+++ fdm_app/views.py
@@ -5,13 +5,68 @@ from .models import Mission, Expense,Technician
 from django.views import View
 from decimal import Decimal
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from django.db.models.functions import TruncMonth,ExtractMonth,ExtractYear
+from django.utils.dateparse import parse_date
+from datetime import datetime
+import calendar
+import re
 
 class MissionListView(View):
-    #obtenir la liste nécessaire pour afficher les missions et les techniciens
+    
+    #obtenir la liste nécessaire pour afficher les missions et les techniciens            
     #pagination de la liste
     def get(self, request, *args, **kwargs):
         # Récupérer toutes les missions
         all_missions = Mission.objects.all().order_by('-id')
+        # Filtrer les missions en fonction de la recherche
+        
+        
+        search_query = request.GET.get('search', '')
+        if search_query:
+            mois_fr = {
+                'janvier': 1, 'février': 2, 'mars': 3, 'avril': 4, 'mai': 5, 'juin': 6,
+                'juillet': 7, 'août': 8, 'septembre': 9, 'octobre': 10, 'novembre': 11, 'décembre': 12
+            }
+            search_lower = search_query.lower()
+            month_number = None
+            for month_name, month_num in mois_fr.items():
+                if month_name.startswith(search_lower):
+                    month_number = month_num
+                    break
+            
+            # Vérifier si c'est une année
+            is_year = search_query.isdigit() and len(search_query) == 4
+            
+            if month_number and is_year:
+                # Si la recherche contient un mois et une année
+                all_missions = all_missions.filter(
+                    Q(start_date__month=month_number, start_date__year=search_query) |
+                    Q(end_date__month=month_number, end_date__year=search_query)
+                )
+            elif month_number:
+                # Si la recherche est seulement un mois
+                all_missions = all_missions.filter(
+                    Q(start_date__month=month_number) |
+                    Q(end_date__month=month_number)
+                )
+            elif is_year:
+                # Si la recherche est seulement une année
+                all_missions = all_missions.filter(
+                    Q(start_date__year=search_query) |
+                    Q(end_date__year=search_query)
+                )
+            else:
+                # Recherche standard
+                all_missions = all_missions.filter(
+                    Q(id__icontains=search_query) |
+                    Q(location__icontains=search_query) |
+                    Q(techniciens__first_name__icontains=search_query) |
+                    Q(techniciens__last_name__icontains=search_query) |
+                    Q(mission_details__icontains=search_query)
+                ).distinct()
+
+        
         paginator = Paginator(all_missions, 5)
         page = request.GET.get('page',1)
         try:
